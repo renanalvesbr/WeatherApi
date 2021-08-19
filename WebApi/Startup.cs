e82f6b9000bedc0626ca1c78f03b5de91ad3cd7b
Renan dos Santos Alves <renan.alves@solutis.com.br>
2021-08-19 17:58:35 -0300

    expose

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -46,9 +46,10 @@ namespace WeatherApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WeatherApi v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WeatherApi v1"));
 
             app.UseHttpsRedirection();
 
